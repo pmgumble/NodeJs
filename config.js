a767e3cf39d3918389ea747072ecb14c25d8aa33
pmgumble <pratikmgumble@gmail.com>
2022-09-14 22:33:53 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1163333017731477',
+        clientSecret: '1e840061f3e78a844547a60c18d93ac7'
+    }
 }
